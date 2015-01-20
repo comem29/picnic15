f9ea618154fc26fd39d44ef0ee87afdbd16f7f58
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2015-01-20 21:11:28 +0100

    install grunt-contrib-watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     sass: {
       dist: {
         options: {
@@ -12,11 +13,25 @@ module.exports = function(grunt) {
           'css/picnic.css': '_sass/picnic.scss'
         }
       }
+    },
+
+    watch: {
+      scripts: {
+        files: ['_sass/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false,
+        },
+      },
     }
+
   });
 
   // Load the plugin that provides the "sass" task.
   grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Load the plugin that provides the "watch" task.
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['sass']);
